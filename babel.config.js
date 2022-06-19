b33d21216b5a28c818cae3e0081f1a3760260db1
为正 <1033460361@qq.com>
2022-06-19 10:24:27 +0800

    babel修改

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   presets: [
     [
-      '@babel/preset-env', {
-        // 默认为false 不对当前js进行填充
-        // usage：依据实际使用的新语法来填充
-        // entry:根据要兼容的浏览器来进行填充（不管实际用没用），不过在设置的时候，需要在入口文件中引入核心包
-            // import "core-js/stable";
-            // import "regenerator-runtime/runtime";
-        useBuiltIns: 'entry',
-        corejs: 3
+      '@babel/preset-env',
+      {
+        /**
+         * useBuiltIns
+         * 默认为false 不对当前js进行填充
+         * usage：按需引入polyfill
+         * entry:需要我们在项目入口处手动引入 polyfill
+         * 需要指定corejs版本
+         */
+        useBuiltIns: 'usage',
+        corejs: 3,
+        targets: {
+          chrome: '65'
+        }
       }
     ]
   ]
