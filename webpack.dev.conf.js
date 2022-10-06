c787fd3defa59d226c11c66db68e035d40a63b10
为正 <1033460361@qq.com>
2022-10-06 10:08:11 +0800

    eslint导出模块报错

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -2,12 +2,8 @@
 const config = require('./config')
 const path = require('path')
 // 利用merge来合并配置
-const {
-  merge
-} = require('webpack-merge');
-const {
-  DefinePlugin
-} = require('webpack')
+const { merge } = require('webpack-merge')
+const { DefinePlugin } = require('webpack')
 
 // 导入公共webpack配置
 const base = require('./webpack.config')
@@ -20,12 +16,12 @@ module.exports = merge(base, {
     // 注意：这里的值，如果是字符串就会被当成表达式，
     // 如果是双重字符串，就会被任务时字符串
     new DefinePlugin({
-      'process.env': JSON.stringify(config.dev.env)
-    })
+      'process.env': JSON.stringify(config.dev.env),
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
